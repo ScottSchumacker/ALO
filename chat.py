24bf35e26be58a90f2407baf65188ecb08987f81
Scott Schumacker <scottschu97@gmail.com>
2022-12-15 00:46:55 -0500

    Created a dev branch as well as a reference file to clean up code

--- chat.py
+++ chat.py
@@ -3,22 +3,7 @@
 
 # Importing libraries
 import random
-
-# Greetings section
-Greetings = ['hello', 'hi', 'hey', "whats up", "what's up", "good morning", "good afternoon",
-             "good evening", "good night", "good day"]
-greetingResponse = ["Hello!", "How are you?", "Hi!", "Hey!", "Whats up?"]
-
-# Name section
-Name = ["whats your name", "what is your name",'your name', 'what are you called by', 'reference number', 'who are you', 'what is your first name']
-nameResponse = ['My name is ALO', 'Hello, I am ALO', "I am ALO", "You can call me ALO", "I'm ALO"]
-
-# Feelings section
-Feeling = ["how are you feeling?", "how are you?", "how do you feel?", "are you happy?", "are you sad?",
-           'are you angry?', "what do you feel?"]
-feelingResponse = ["I am doing well", "I have been better", "I am great!", "I am happy", "I am sad"]
-
-# Likes = []
+import reference
 
 # Creating a variable to leave the chat
 leave = "bye"
@@ -29,9 +14,9 @@ while True:
     if inp == leave:
         print("Good bye. I hope to chat soon!")
         break
-    elif str(inp) in Greetings:
-        print(random.choice(greetingResponse))
-    elif str(inp) in Name:
-        print(random.choice(nameResponse))
-    elif str(inp) in Feeling:
-        print(random.choice(feelingResponse))
+    elif str(inp) in reference.Greetings:
+        print(random.choice(reference.greetingResponse))
+    elif str(inp) in reference.Name:
+        print(random.choice(reference.nameResponse))
+    elif str(inp) in reference.Feeling:
+        print(random.choice(reference.feelingResponse))
---
+++ reference.py
@@ -0,0 +1,18 @@
+# Scott Schumacker
+# Reference file
+
+# Greetings section
+Greetings = ['hello', 'hi', 'hey', "whats up", "what's up", "good morning", "good afternoon",
+             "good evening", "good night", "good day"]
+greetingResponse = ["Hello!", "How are you?", "Hi!", "Hey!", "Whats up?"]
+
+# Name section
+Name = ["whats your name", "what is your name",'your name', 'what are you called by', 'reference number', 'who are you', 'what is your first name']
+nameResponse = ['My name is ALO', 'Hello, I am ALO', "I am ALO", "You can call me ALO", "I'm ALO"]
+
+# Feelings section
+Feeling = ["how are you feeling?", "how are you?", "how do you feel?", "are you happy?", "are you sad?",
+           'are you angry?', "what do you feel?"]
+feelingResponse = ["I am doing well", "I have been better", "I am great!", "I am happy", "I am sad"]
+
+# Likes = []
